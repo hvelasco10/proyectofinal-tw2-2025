17f2e07c88c462fa8b3274199f474768df92289b
humber <totobot2014@gmail.com>
2025-10-20 19:53:03 -0600

    Segundo commit, creado el controlador de acceso, plantilla base de acceso y vista de acceso

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\AccesoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [AccesoController::class, 'mostrarFormulario'])->name('acceso');
+Route::post('/login', [AccesoController::class, 'iniciarSesion'])->name('login');
